6a590a8c25159f42cd8ee5a7608b89c6c9d22453
渡邉舜也 <shun.1630.effort@gmail.com>
2019-06-12 13:56:54 +0900

    削除機能を追加しました。
    DELETEを押すとそのタスクが消せるようになりました。

--- Models/Todo.php
+++ Models/Todo.php
@@ -41,4 +41,10 @@ class Todo
         prepare('UPDATE ' . $this->table .' SET name = ? WHERE id = ?');
         $stmt->execute([$name,$id]);
     }
+
+    public function delete($id){
+        $stmt = $this->db_manager->dbh->
+        prepare('DELETE FROM ' . $this->table . ' WHERE id = ?');
+        $stmt->execute([$id]);
+    }
 }
